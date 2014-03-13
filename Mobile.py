05c156a499db3667f91178aa1cde32c131455420
Letick <thom0294@hotmail.fr>
2014-03-13 22:25:20 +0100

    Premières classes

---
+++ Mobile.py
@@ -0,0 +1,3 @@
+class Mobile:
+	def __init__(self, poids=0):
+		self.poids = poids
---
+++ Noeud.py
@@ -0,0 +1,6 @@
+class Noeud:
+	"Défini un noeud"
+	def __init__(self, n_droit=null, n_gauche=null):
+		self.n_droit=n_droit
+		self.n_droit=n_gauche
+
