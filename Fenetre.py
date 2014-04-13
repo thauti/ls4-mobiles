a9263843929732de19326a19fbc6e498a69e23b9
Letick <thom0294@hotmail.fr>
2014-04-13 15:48:53 +0200

    Nettoyage du code et scroll !

--- Fenetre.py
+++ Fenetre.py
@@ -7,16 +7,28 @@ class Fenetre(Tk): #Héritage depuis Tk
 		
 		Tk.__init__(self) #Constructeur de la classe Hérité
 		self.title("Construction du mobile")
-
 		self.menubar = Menu(self)
+		
+		self.frame = Frame(self, bd=0)
+
 		self.menufichier = Menu(self.menubar, tearoff=0)
 		self.menufichier.add_command(label="Ouvrir", command=self.ouvrir_ficher)
 		self.menufichier.add_command(label="Quitter", command=self.destroy)
 		self.menubar.add_cascade(label="Fichier", menu=self.menufichier)
 
-		self.canvas = Canvas(self, width=1000, height=1000, bg="white")
-		self.canvas.pack()
+		self.canvas = Canvas(self.frame, width=800, height=800, bg="white", scrollregion=(0,0,1500,1000))
+		self.xscrollbar = Scrollbar(self.frame, orient=HORIZONTAL, command=self.canvas.xview)
+		self.yscrollbar = Scrollbar(self.frame, command=self.canvas.yview)
+		
+		self.canvas.configure(xscrollcommand=self.xscrollbar.set,yscrollcommand=self.yscrollbar.set)
+
+		self.xscrollbar.pack(side="top",fill=BOTH,  expand=0)
+		self.yscrollbar.pack(side="right",fill=BOTH,  expand=0)
+
 		self.config(menu=self.menubar)
+
+		self.frame.pack(fill=BOTH, expand=1)
+		self.canvas.pack(fill=BOTH, expand=1)
 
 	def ouvrir_ficher(self):
 
@@ -32,20 +44,17 @@ class Fenetre(Tk): #Héritage depuis Tk
 
 		#### AMELIORABLE ####
 		premier_chara = fichier.read(1) # On lit le premier charactère de la première ligne
-		texte =  fichier.readlines() # On récupère le fichire en liste
+		texte =  fichier.readlines() # On récupère le fichier en liste
 		texte = [line.rstrip() for line in texte]
 		texte[0] = premier_chara+texte[0] # On remet le premier charactère au début
-
-		if premier_chara == '[' : # Si il est du type Arbre
-			self.convertir_en_arbre(texte)
-		elif int(premier_chara) in range(0,10): # Si c'est un ensemble de poids
-			arbre_1 = self.mode_1(texte)
-			self.arbre = Arbre()
-			self.arbre.construire_fichier_arbre(arbre_1)
-			self.ecrire_arbre(arbre_1)
-			
-			
-		else:							# Sinon erreur
+		try:
+			if premier_chara == '[' : # Si il est du type Arbre
+				self.convertir_en_arbre(texte)
+			elif int(premier_chara) in range(0,10): # Si c'est un ensemble de poids
+				arbre_1 = self.mode_1(texte)
+				self.arbre = Arbre()
+				self.arbre.construire_fichier_arbre(arbre_1)
+		except:							# Sinon erreur
 			print("Erreur: Ce fichier n'est pas valide")
 		fichier.close()
 
@@ -70,32 +79,3 @@ class Fenetre(Tk): #Héritage depuis Tk
 			return [self.mode_1(liste[:milieu]),self.mode_1(liste[milieu:])]
 
 	#def mode_2(self,liste) :
-	
-	def ecrire_arbre(self,arbre) :
-		
-		fichier = open("ArbrePoids.txt","w")
-		res="["
-		res = res+self.traitement_ecriture(arbre)		
-		res= res +"]"
-		fichier.write(res)
-		fichier.close()
-
-
-	def traitement_ecriture(self,arbre) :
-		res=""
-		if type(arbre[0]) is not list :
-
-			res=res+arbre[0]+","
-			
-		if type(arbre[1]) is not list :
-			print(arbre[1])
-			res=res+arbre[1]
-		
-		if type(arbre[0]) is list :
-			print(arbre[0])
-			res=res+"["+self.traitement_ecriture(arbre[0])+"],"
-			
-		if type(arbre[1]) is list :			
-			res = res +"["+self.traitement_ecriture(arbre[1])+"]"		
-		
-		return res
--- Projet.py
+++ Projet.py
@@ -4,35 +4,6 @@ from Arbre import *
 from Fenetre import *
 from tkinter import *
 
-""" ESSAI(test)
-monArbre = Arbre()
-noeud = Noeud()
-noeud2 = Noeud()
-
-mobile1 = Mobile(8)
-mobile2 = Mobile(5)
-mobile3 = Mobile(3)
-
-noeud2.n_gauche = mobile2
-noeud2.n_droit = mobile3
-noeud.n_droit = mobile1
-noeud.n_gauche = noeud2
-
-monArbre.noeud = noeud
-
-"""
-
-""" 
-Represente
-
-		X
-	/		\
-	X		8
-  /	 \
- 5	  3
- """
-
-""" ESSAI 2 """
 
 Fen = Fenetre()
 Fen.mainloop()
